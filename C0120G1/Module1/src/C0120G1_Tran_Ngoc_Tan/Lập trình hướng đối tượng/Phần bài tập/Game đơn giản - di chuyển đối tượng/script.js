0310d068162eb9b719ede33dc7238ea8add673b7
tranngoctan <tranngoctan16t5@gmail.com>
2020-02-05 11:38:21 +0700

    hihi

--- C0120G1/Module1/src/C0120G1_Tran_Ngoc_Tan/Lập trình hướng đối tượng/Phần bài tập/Game đơn giản - di chuyển đối tượng/script.js
+++ C0120G1/Module1/src/C0120G1_Tran_Ngoc_Tan/Lập trình hướng đối tượng/Phần bài tập/Game đơn giản - di chuyển đối tượng/script.js
@@ -29,6 +29,8 @@ function Hero(image,top,left,size) {
     }
 }
 var hero = new Hero('pikachu.png', 20, 30, 200);
+var hero1 = new Hero('pikachu.png', 20, 200, 200);
+var hero2 = new Hero('pikachu.png', 20, 200, 200);
 
 function start(){
     if(hero.left < window.innerWidth - hero.size && hero.top===20){
@@ -48,7 +50,54 @@ function start(){
 
 
     document.getElementById('game').innerHTML = hero.getHeroElement();
+    // document.getElementById('game1').innerHTML = hero1.getHeroElement();
     setTimeout(start, 50);
 }
 
 start();
+function start1(){
+    if(hero1.left < window.innerWidth - hero1.size && hero1.top===20){
+        console.log(hero1.left);
+        hero1.moveRight();
+    }
+    else if(hero1.top < window.innerHeight - hero1.size && hero1.left>window.innerWidth - hero1.size){
+        hero1.moveBottom();
+    }else if(hero1.left > window.innerWidth - hero1.size){
+        hero1.moveLeftNguoc();
+    }
+    else if(hero1.left > 20){
+        hero1.moveLeftNguoc();
+    }else {
+        hero1.moveTopNguoc();
+    }
+
+
+    // document.getElementById('game').innerHTML = hero.getHeroElement();
+    document.getElementById('game1').innerHTML = hero1.getHeroElement();
+    setTimeout(start1, 70);
+}
+
+start1();
+function start2(){
+    if(hero2.left < window.innerWidth - hero2.size && hero2.top===20){
+        console.log(hero2.left);
+        hero2.moveRight();
+    }
+    else if(hero2.top < window.innerHeight - hero2.size && hero2.left>window.innerWidth - hero2.size){
+        hero2.moveBottom();
+    }else if(hero2.left > window.innerWidth - hero2.size){
+        hero2.moveLeftNguoc();
+    }
+    else if(hero2.left > 20){
+        hero2.moveLeftNguoc();
+    }else {
+        hero2.moveTopNguoc();
+    }
+
+
+    document.getElementById('game2').innerHTML = hero2.getHeroElement();
+    // document.getElementById('game1').innerHTML = hero1.getHeroElement();
+    setTimeout(start2, 400);
+}
+
+start2();
