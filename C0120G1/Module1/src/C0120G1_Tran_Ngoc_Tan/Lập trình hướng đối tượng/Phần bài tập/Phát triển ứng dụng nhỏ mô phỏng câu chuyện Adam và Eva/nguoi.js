3fe5787eedf499ce990b0c63d51292734110edda
tranngoctan <tranngoctan16t5@gmail.com>
2020-02-13 06:57:52 +0700

    hihi

--- C0120G1/Module1/src/C0120G1_Tran_Ngoc_Tan/Lập trình hướng đối tượng/Phần bài tập/Phát triển ứng dụng nhỏ mô phỏng câu chuyện Adam và Eva/nguoi.js
+++ C0120G1/Module1/src/C0120G1_Tran_Ngoc_Tan/Lập trình hướng đối tượng/Phần bài tập/Phát triển ứng dụng nhỏ mô phỏng câu chuyện Adam và Eva/nguoi.js
@@ -1,74 +1,79 @@
-var Human=function (name,age,weight) {
-    this.name=name;
-    this.weight=weight;
-    this.age=age;
+var Human = function (name, age, weight) {
+    this.name = name;
+    this.weight = weight;
+    this.age = age;
     // this.gender=gender;
 
     // this.setName=function (name) {
     //     this.name=name;
     // };
-    this.getName=function () {
+    this.getName = function () {
         return this.name;
     };
 
-    this.setGender=function (gender) {
-        this.gender=gender;
+    this.setGender = function (gender) {
+        this.gender = gender;
     };
-    this.getGender=function () {
+    this.getGender = function () {
         return this.gender;
     };
-    this.setAge=function (age) {
-        this.age=age;
+    this.setAge = function (age) {
+        this.age = age;
     };
-    this.getAge=function () {
+    this.getAge = function () {
         return this.age;
     };
-    this.setWeight=function (weight) {
-        this.weight=weight;
+    this.setWeight = function (weight) {
+        this.weight = weight;
     };
-    this.getWeight=function () {
+    this.getWeight = function () {
         return this.weight;
     };
 
-    this.checkGender=function () {
+    this.checkGender = function () {
         if (this.gender == true) {
             return "Giới tính Nam";
         } else {
             return "Giới tính Nữ";
         }
     }
-   this.isMale=function () {
-       return this.gender=true;
-   };
-    this.isFemale=function () {
-        return this.gender=false;
+    this.isMale = function () {
+        return this.gender = true;
     };
-    this.eatTao=function () {
-        this.weight++;
-        tao.weight--;
+    this.isFemale = function () {
+        return this.gender = false;
     };
-    this.say=function (noiDung) {
+    this.eatTao = function () {
+        if (tao.getWeight() > 0) {
+            this.weight++;
+            tao.weight--;
+        } else {
+            return;
+        }
+
+    };
+    this.say = function (noiDung) {
         return noiDung;
     };
-    this.kiemTraTao=function () {
+    this.kiemTraTao = function () {
         return tao.getWeight();
     }
 
 }
-var Tao=function (weight) {
-    this.weight=weight;
+var Tao = function (weight) {
+    this.weight = weight;
 
-    this.getWeight=function () {
+    this.getWeight = function () {
         return this.weight;
     };
 
-    this.decrease=function () {
+    this.decrease = function () {
         this.weight--;
     };
-    this.isEmpty=function () {
-        if(this.getWeight()>=0){
+    this.isEmpty = function () {
+        if (this.getWeight() >0) {
             return true;
-        }else {
+        } else {
             return false;
         }
     }
