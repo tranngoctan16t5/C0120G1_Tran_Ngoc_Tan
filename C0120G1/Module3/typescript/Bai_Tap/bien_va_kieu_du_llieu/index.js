dcace956f8376ae8835fd266f974170785e43f82
tranngoctan16t5 <49084352+tranngoctan16t5@users.noreply.github.com>
2020-05-12 21:29:32 +0700

    haha

---
+++ C0120G1/Module3/typescript/Bai_Tap/bien_va_kieu_du_llieu/index.js
@@ -0,0 +1,11 @@
+function main() {
+    var x = 5;
+    console.log(x);
+    var x = 10;
+    console.log(x);
+    // let y = 55;
+    // console.log(y);
+    // let y = 100;
+    // console.log(y);
+}
+main();
